3a708551875dafec394379f1e0f411f3d26dad1c
Peter Cervienka <peter.cervienka@erni.sk>
2015-02-13 16:08:12 +0100

    edit java version

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -3,8 +3,8 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.work.dir = "target/work"
-grails.project.target.level = 1.6
-grails.project.source.level = 1.6
+grails.project.target.level = 1.7
+grails.project.source.level = 1.7
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.reload.enabled = true
@@ -71,6 +71,9 @@ grails.project.dependency.resolution = {
 
         // plugin for charts visualization
         compile ":google-visualization:1.0"
+
+        // plugin for rich UI
+        // compile ":richui:0.8"
 
         // Uncomment these to enable additional asset-pipeline capabilities
         //compile ":sass-asset-pipeline:1.9.0"
